7edac085bc79c70552b853b0fae14688f8c1b43e
Fardin-E <fardinrahman647@gmail.com>
2024-11-21 17:23:41 -0500

    created the shader buffer

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,13 @@
 
 void processInput(GLFWwindow* window);
 
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
 int main(int argc, char** argv) {
 
 	//initialize
@@ -31,6 +38,26 @@ int main(int argc, char** argv) {
 	glViewport(0, 0, 800, 600);
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	// Vertex input
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// compiling the Vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
 
 	while (!glfwWindowShouldClose(window)) {
 		//input
