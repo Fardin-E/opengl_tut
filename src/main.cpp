ada10c5fa8279546c8bcf818bf600570c6f112f7
Fardin Rahman <71291566+Fardin-E@users.noreply.github.com>
2024-11-24 14:15:30 -0500

    rainbow triangle.

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,26 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "callback.h"
+#include "error_checking.h"
 
 void processInput(GLFWwindow* window);
 
-const char *vertexShaderSource = "#version 330 core\n"
+const char *vertexShaderSource = "#version 430 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"	gl_Position = vec4(aPos, 1.0);\n"
+"	ourColor = aColor;\n"
 "}\0";
 
-const char *fragmentShaderSource = "#version 330 core\n"
+const char *fragmentShaderSource = "#version 430 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+"	FragColor = vec4(ourColor, 1.0);\n"
 "}\n";
 
 int main(int argc, char** argv) {
@@ -24,10 +29,11 @@ int main(int argc, char** argv) {
 	//initialize
 	glfwInit();
 
-	//set options of opengl version. its set to opengl 3.3
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	//set options of opengl version. its set to opengl 4.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	// create window
@@ -43,6 +49,16 @@ int main(int argc, char** argv) {
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
+	}
+
+	// enable OpenGL debug context if context allows for debug context
+	int flags; glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+	if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+	{
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS); // makes sure errors are displayed synchronously
+		glDebugMessageCallback(glDebugOutput, nullptr);
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
 	}
 
 	glViewport(0, 0, 800, 600);
@@ -91,22 +107,29 @@ int main(int argc, char** argv) {
 	glDeleteShader(fragmentShader);
 
 	// Vertex input
-	float vertices[] = {
-	 0.5f,  0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	-0.5f, -0.5f, 0.0f,
-	-0.5f,  0.5f, 0.0f
+	/*float vertices[] = {
+	 0.9f,  0.5f, 0.0f,
+	 0.9f, -0.5f, 0.0f,
+	-0.1f, -0.5f, 0.0f,
+	-0.1f,  0.5f, 0.0f
 	};
 	unsigned int indices[] = {
 		0, 1, 3,
 		1, 2, 3
+	};*/
+
+	float vertices[] = {
+		// positions		// colors
+		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+	   -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
+		0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
 	};
 
 	// initialize and generate buffers
-	unsigned int VAO, VBO, EBO;
+	unsigned int VAO, VBO; /*, EBO;*/
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	/*glGenBuffers(1, &EBO);*/
 	// bind the vertex array object first, then bind and set vertex buffer(s), 
 	// then configure vertex attributes
 	glBindVertexArray(VAO);
@@ -114,14 +137,19 @@ int main(int argc, char** argv) {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	/*glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);*/
 	
 	// Linking vertex attributes
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glUseProgram(shaderProgram);
 
 	while (!glfwWindowShouldClose(window)) {
 		//input
@@ -130,15 +158,23 @@ int main(int argc, char** argv) {
 		// rendering commands here
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		// activate the shader program
+		/*glUseProgram(shaderProgram);*/
 
-		glUseProgram(shaderProgram);
+		//update the object
+
+		// render the object 
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 	glfwTerminate();
 
